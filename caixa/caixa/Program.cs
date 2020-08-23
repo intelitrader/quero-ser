9f70b9e4f1c4cbf879a47c4e8f555566af077d12
JC-Jose <jose.cjunior@netshoes.com>
2020-08-23 16:43:30 -0300

    up

---
+++ caixa/caixa/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace caixa
+{
+    public class Caixa
+    {
+        public string saque(int v)
+        {
+            int[] notas = { 100, 50, 20, 10 };
+            int[] aux = new int[4];
+            string resultado = "";
+            int resto;
+
+            if (v <= 0)
+            {
+                resultado = "Valor inválido";
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (v >= notas[i])
+                {
+                    aux[i] = v / notas[i];
+                    v = v % notas[i];
+                }
+            }
+            for(int i = 0; i<aux.Length;i++)
+            {
+                if (aux[i] !=0 && aux[i] >= 1)
+                {
+                    resultado += "notade" + notas[i].ToString() + ":" + aux[i].ToString();
+                }
+            }
+            if (v <= 0)
+                return resultado;
+            else
+                return "Valor indisponível";
+                
+        } 
+      
+    }
+}
---
+++ media/media/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valores = 6 + 70 + 3 + 9;
+
+            double divisor = 4;
+            double resultado = valores/divisor;
+
+            Console.WriteLine("A média do valor é: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
