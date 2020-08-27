f8458244027e42ed9d53ffa0568ff7f410a71b03
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-08-27 12:11:00 -0300

    Termina o programa
    O desafio pede números mínimo, máximo, quantidade de elementos e não uma lista fixa. Esse commit corrige tudo isso e formata a saída conforme o exemplo do exercício.

--- media/media/Program.cs
+++ media/media/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace media
 {
@@ -6,11 +6,24 @@ namespace media
     {
         static void Main(string[] args)
         {
-            double valores = 6 + 70 + 3 + 9;
+            int total = 0, minimo = int.MaxValue, maximo = int.MinValue;
 
-            double divisor = 4;
-            double resultado = valores/divisor;
+            Console.WriteLine("Digite os elementos separados por espaço seguido de nova linha:");
+            string[] ss = Console.ReadLine().Split(' ');
 
+            foreach( var s in ss )
+            {
+                int atual = int.Parse(s);
+                maximo = System.Math.Max(maximo, atual);
+                minimo = System.Math.Min(minimo, atual);
+                total += atual;
+            }
+
+            double resultado = Convert.ToDouble(total) / Convert.ToDouble(ss.Length);
+
+            Console.WriteLine("Valor mínimo: " + minimo);
+            Console.WriteLine("Valor máximo: " + maximo);
+            Console.WriteLine("Número de elementos na seqüência: " + ss.Length);
             Console.WriteLine("A média do valor é: " + resultado);
             Console.ReadKey();
 
