2565b8c3b5816f063b980220eabc26a5ffcbbf83
Gu-R <gustavo.araujocount@outlook.com>
2020-09-17 20:47:24 -0300

    commit inicial

---
+++ Teste-1/Teste1/Teste1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("A soma dos numeros é: " + (num1 + num2));
+
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+    }
+}
