028bf5bb151e4dc6c8e604b5551ae984e37919e0
apocryphoon <ricardo.rc15@hotmail.com>
2020-09-25 00:47:30 +0000

    Exercicio 2
    Adicionado o exercicio referente a Geração de Fatores Primos
    proposto pela Intelitrader.

---
+++ FatorPrimo/FatorPrimo/Calculo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorPrimo
+{
+    public static class Calculo
+    {
+        const int MAX_SIZE = 15000;
+
+        public static bool IsNumeroPrimo(int numero)
+        {
+            bool bPrimo = true;
+            int fator = numero / 2;
+            int i = 0;
+            for (i = 2; i <= fator; i++)
+            {
+                if ((numero % i) == 0)
+                    bPrimo = false;
+            }
+            return bPrimo;
+        }
+        public static int GetFatoresPrimos(int numero, out int[] arrResultado)
+        {
+            int contador = 0;
+            int[] vetor = new int[MAX_SIZE];
+            arrResultado = new int[MAX_SIZE];
+            int i = 0;
+            int indice = 0;
+            for (i = 2; i <= numero; i++)
+            {
+                if (IsNumeroPrimo(i) == true)
+                    vetor[contador++] = i;
+            }
+            while (true)
+            {
+                if (IsNumeroPrimo(numero) == true)
+                {
+                    arrResultado[indice++] = numero;
+                    break;
+                }
+                for (i = contador - 1; i >= 0; i--)
+                {
+                    if ((numero % vetor[i]) == 0)
+                    {
+                        arrResultado[indice++] = vetor[i];
+                        numero = numero / vetor[i];
+                        break;
+                    }
+                }
+            }
+            return indice;
+        }
+    }
+}
---
+++ FatorPrimo/FatorPrimo/frmFatores.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FatorPrimo
+{
+    public partial class frmFatores : Form
+    {
+        public frmFatores()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int numero = Convert.ToInt32(textBox1.Text);
+
+            int contador = Calculo.GetFatoresPrimos(numero, out int[] arrResultado);            
+
+            for (int i = 0; i < contador; i++)
+            {
+                listBox1.Items.Add(arrResultado[i]);
+                if (i != contador - 1)
+                {
+                    int teste1 = arrResultado[i];
+
+                    listBox1.Items.Add("------");
+                    //listBox1.Items.Add(teste1).ToString("{0}");
+                    listBox1.Items.Add(string.Format("{0} {1}", arrResultado[i], "x"));
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://github.com/Apocryphoon");
+            Process.Start(sInfo);
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            pictureBox2.ClientSize = new Size(163, 90);
+            pictureBox2.Location = new Point(63, 24);
+            pictureBox2.Image = Properties.Resources.rubrica;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
