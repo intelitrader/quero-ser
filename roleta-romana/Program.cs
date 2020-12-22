a3630fa0a65e6edfdb5af2bcfe29268ac5e72402
Thiago <henriquesilva322@outlook.com>
2020-12-22 18:47:29 -0300

    desafios-Thiago Henrique

---
+++ roleta-romana/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace roleta_romana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                --ROLETA ROMANA--
+                https://dojopuzzles.com/problems/roleta-romana/
+                número de pessoas : quantidade > 0 
+                posição inicial : 1 <= quem começa <= quantidade
+                número de passos: de quantas em quantas > 0
+
+            */
+            
+            
+            int pessoas = 5;
+            int passo = 2;
+            int inicio = 1;
+
+            int count = 1;
+            int proximo = inicio + passo;
+
+      
+
+            do{
+                Console.WriteLine(proximo.ToString());
+
+                
+                proximo = proximo + passo;
+
+                
+                if (proximo > pessoas)
+                {
+                    proximo = proximo - pessoas;
+                }
+
+                count++;
+            }while(count < pessoas);
+
+        }
+    }
+}
---
+++ vetores-escalar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace vetores_escalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            /*
+                    --PRODUTO ESCALAR DE VETORES--
+                https://dojopuzzles.com/problems/produto-escalar-de-vetores/
+            */
+            int multiplica=0, resultado = 0, somar = 0;
+            int tamanhoA, tamanhoB;
+            string resposta;
+
+            Console.WriteLine("Digite um nº:");
+            tamanhoA = int.Parse(Console.ReadLine());
+            tamanhoB = tamanhoA;
+
+            int [] vetorA = new int[tamanhoA];
+
+            int [] vetorB = new int[tamanhoB];
+
+            for(int i = 0; i <=vetorA.Length-1;i++){
+                
+                    Console.WriteLine($"Adicione {i} valor:");
+                    vetorA[i] = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine($"Adicione {i} outro valor:");
+                    vetorB[i] = int.Parse(Console.ReadLine());
+
+                    
+
+                    multiplica = vetorA[i] * vetorB[i];
+
+                    somar += multiplica;
+                   
+
+                    resultado = resultado + somar;
+                    
+            }
+
+            Console.ReadKey();
+
+
+            if(resultado == 0){
+                resposta = "Ângulo reto";
+            }
+            else if (resultado < 0){
+                resposta = "Ângulo obtuso";
+            }
+            else{
+                resposta = "Ângulo agudo";
+            }
+            
+            Console.WriteLine("O produto escalar de vetores é: " + resultado +"/"+ resposta);
+
+        }
+    }
+}
