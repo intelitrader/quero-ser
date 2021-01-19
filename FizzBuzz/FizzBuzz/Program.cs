3c388d4cef0d170ecdcc6530b7468ad7ea87e1fd
devjoaosantos <joao.santos.farias.2003@gmail.com>
2021-01-19 16:00:44 -0300

    Subindo Desafios

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FIZZBUZZ");
+            Console.WriteLine("Iremos exibir números de 0 à 100");
+            Console.WriteLine("Quando for um número divisível por 3, irá aparecer 'FIZZ'");
+            Console.WriteLine("Quando for um número divisível por 5, irá aparecer 'BUZZ'");
+            Console.WriteLine("Quando for um número divisível por 3 e 5, irá aparecer 'FIZZBUZZ'");
+
+            //Contagem de 0 a 100
+            for(int i = 1; i < 101; i++)
+            {
+                //Caso seja divisível por 3 e 5
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FIZZBUZZ");
+                }
+                //Caso seja divisível por 3
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                //Caso seja divisível por 5
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                //Caso não seja divisível por 3 ou 5
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
