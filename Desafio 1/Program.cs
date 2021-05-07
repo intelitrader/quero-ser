d7dea56e0a0159ffa26c6815f3db02d99d4750af
Luis Fernando <luisfernando20140@outlook.com>
2021-05-07 03:24:57 -0300

    Desafios iniciados

---
+++ Desafio 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Desafio 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
