e18eb88d922b5201e5de7b078347fa5f3f5c020b
Luis Fernando <luisfernando20140@outlook.com>
2021-05-07 03:37:31 -0300

    Desafio 3 concluido

--- Desafio 3/Program.cs
+++ Desafio 3/Program.cs
@@ -6,7 +6,33 @@ namespace Desafio_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Console.Clear();
+            /*
+        FEITO POR LUIS FERNANDO DE MESQUITA PEREIRA
+        DESAFIO DOJO
+         Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
+         Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
+         Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
+        */
+        Console.Clear();
+        for(int i = 1; i < 101; i++){
+            if(i % 5 == 0 && i % 3 == 0){
+                Console.Write("FizzBuzz |");
+            }
+            else{
+                if(i % 3 == 0){
+                    Console.Write("Fizz |");
+                }
+                else{
+                    if(i % 5 == 0){
+                        Console.Write("Buzz |");
+                    }
+                    else{
+                        Console.Write(i + " |");
+                    }
+                }
+            }
+        }
         }
     }
 }
