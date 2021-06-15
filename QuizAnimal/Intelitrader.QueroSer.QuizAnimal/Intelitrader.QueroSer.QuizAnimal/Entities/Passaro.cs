88b93986162af15cd25ad28b71c78bd4e7608957
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2021-06-15 12:55:14 -0300

    Rodrigo Furlaneti

---
+++ QuizAnimal/Intelitrader.QueroSer.QuizAnimal/Intelitrader.QueroSer.QuizAnimal/Entities/Passaro.cs
@@ -0,0 +1,13 @@
+﻿namespace Intelitrader.QueroSer.QuizAnimal.Entities
+{
+    public class Passaro : Animal
+    {
+        public Passaro()
+        {
+            Raca = "Passaro";
+            Costume = "Alegre";
+            Alimentacao = "Fruta";
+            Locomocao = "Voa";
+        }
+    }
+}
