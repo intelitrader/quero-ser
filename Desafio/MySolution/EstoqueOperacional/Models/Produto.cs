c80cb15fc9fbf80980de7b5c6da17a65a8a78865
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2021-07-14 17:18:56 -0300

    Criação dos modelos Produto e Venda que representam, respectivamente, instâncias de um produto e venda

---
+++ Desafio/MySolution/EstoqueOperacional/Models/Produto.cs
@@ -0,0 +1,9 @@
+namespace EstoqueOperacional.Models
+{
+    public class Produto
+    {
+        public int codigo {get ; set;}
+        public int qtdEstoque {get;set;}
+        public int qtdMinCO {get;set;}
+    }
+}
---
+++ Desafio/MySolution/EstoqueOperacional/Models/Venda.cs
@@ -0,0 +1,10 @@
+namespace EstoqueOperacional.Models
+{
+    public class Venda
+    {
+        public int codigo {get;set;}
+        public int qtdVendida {get;set;}
+        public int situacao {get;set;}
+        public int canal {get;set;}
+    }
+}
