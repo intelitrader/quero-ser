b442a9750ae47de772a659e5b66a5de2ec320043
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2021-07-20 01:35:16 -0300

    Validação na propriedade 'Produto.codigo' feita

--- Desafio/MySolution/EstoqueOperacional/Models/Produto.cs
+++ Desafio/MySolution/EstoqueOperacional/Models/Produto.cs
@@ -2,7 +2,20 @@ namespace EstoqueOperacional.Models
 {
     public class Produto
     {
-        public int codigo {get ; set;}
+        private int _codigo;
+        public int codigo
+        {
+            get => _codigo;
+            
+            set {
+                if(!(value >= 10000) && !(value <= 99999))
+                {
+                    _codigo = 0;
+                }
+
+                _codigo = value;
+            }
+        }
         public int qtdEstoque {get;set;}
         public int qtdMinCO {get;set;}
 
