c8bd5c62b1fdc357fb7b89629c1aced7213833c9
Igor Souza <souza.igor@protonmail.com>
2021-08-05 00:27:38 -0300

    Creating blank project.

---
+++ Desafio/MySolution/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MySolution.Models;
+
+namespace MySolution
+{
+    class Program
+    {
+        
+    }
+}
