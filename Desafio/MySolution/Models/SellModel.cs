57cbab86cd2f5dc839dfbb1893683fdb63b19b33
Igor Souza <souza.igor@protonmail.com>
2021-08-05 03:52:02 -0300

    Updating SellModel.

--- Desafio/MySolution/Models/SellModel.cs
+++ Desafio/MySolution/Models/SellModel.cs
@@ -2,9 +2,18 @@ namespace MySolution.Models
 {
     public class SellModel
     {
+        public SellModel(int code, int quantity, StatusModel status, ChannelModel channel)
+        {
+            Code = code;
+            Quantity = quantity;
+            Status = status;
+            Channel = channel;
+        }
+
         public int Code { get; set; }
         public int Quantity { get; set; }
         public StatusModel Status{ get; set; }
         public ChannelModel Channel{ get; set; }
+
     }
 }
