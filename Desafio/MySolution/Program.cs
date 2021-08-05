4c8dc3e6aa6d3a265b2a9587b4ce3723b98f332f
Igor Souza <souza.igor@protonmail.com>
2021-08-05 00:33:38 -0300

    Initializing lists and applying simple validation to input files.

--- Desafio/MySolution/Program.cs
+++ Desafio/MySolution/Program.cs
@@ -9,6 +9,22 @@ namespace MySolution
 {
     class Program
     {
-        
+        public static List<SellModel> sells = new ();
+        public static List<SellModel> products = new ();
+        static void Main(string[] args)
+        {
+            string[] argss = new string[]{
+                @"C:\Users\dev\Documents\GitHub\quero-ser\Desafio\MySolution\bin\Debug\net5.0\c1_vendas.txt",
+                @"C:\Users\dev\Documents\GitHub\quero-ser\Desafio\MySolution\bin\Debug\net5.0\c1_produtos.txt"};
+            
+            if(argss.Length != 2)
+            {
+                Console.WriteLine("Modo de usar:");
+                Console.WriteLine("mysolution.exe vendas.txt produtos.txt");
+                return;
+            }
+
+        }
     }
+        
 }
