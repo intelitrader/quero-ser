2d80b163ebfff981f5f39ef0fb523f0e410b6809
Igor Souza <souza.igor@protonmail.com>
2021-08-31 23:30:49 -0300

    Create DivergenciesReport.cs

---
+++ Desafio/MySolution/Reports/Reports/DivergenciesReport.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace MySolution.Models
+{
+    static class DivergenciesReport
+    {
+        public static void Save(IEnumerable<SellModel> sells, IEnumerable<ProductModel> products)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            Dictionary<uint, ProductModel> productsD = products.ToDictionary(x => x.ProdCode);
+
+            int i = 0;
+
+            foreach (var item in sells)
+            {
+                if (item.Status == (StatusModel)999)
+                {
+                    sb.Append($"Linha {i+1} – Erro desconhecido. Acionar equipe de TI\n");
+                }
+                else if (!productsD.ContainsKey(item.ProdCode))
+                {
+                    sb.Append($"Linha {i+1} – Código de Produto não encontrado {item.ProdCode}\n");
+                }
+                else if (item.Status == (StatusModel)135)
+                {
+                    sb.Append($"Linha {i+1} – Venda cancelada\n");
+                }
+                else if (item.Status == (StatusModel)190)
+                {
+                    sb.Append($"Linha {i+1} – Venda não finalizada\n");
+                }
+                i++;
+            }
+
+            var content = sb.ToString();
+            File.WriteAllText("DIVERGENCIAS.txt", content);
+            
+        }
+        
+        
+    }
+}
