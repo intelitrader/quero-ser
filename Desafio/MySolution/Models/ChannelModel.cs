dbbc677e7f6e9f15435013f4d8c1608aa24aae69
Igor Souza <souza.igor@protonmail.com>
2021-08-31 22:57:52 -0300

    Update ChannelModel now has an extension method

--- Desafio/MySolution/Models/ChannelModel.cs
+++ Desafio/MySolution/Models/ChannelModel.cs
@@ -7,4 +7,23 @@ namespace MySolution.Models
         AndroidApp,
         IphoneApp
     }
+    public static class ChannelExtensions
+    {
+    public static string ToFriendlyString(this Channel channel)
+        {
+            switch(channel)
+            {
+                case Channel.Representative:
+                    return "Representantes";
+                case Channel.Website:
+                    return "Website";
+                case Channel.Android:
+                    return "App móvel Android";
+                case Channel.Iphone:
+                    return "App móvel iPhone";
+                default:
+                    return "";
+            }
+        }
+    }
 }
