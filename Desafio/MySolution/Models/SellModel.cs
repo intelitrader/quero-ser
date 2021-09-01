ba9b4797e3ac1208a708c926619be2353c89275b
Igor Souza <souza.igor@protonmail.com>
2021-08-31 23:09:48 -0300

    Update count sold by channel and prodcode is now done at model creation

--- Desafio/MySolution/Models/SellModel.cs
+++ Desafio/MySolution/Models/SellModel.cs
@@ -2,18 +2,28 @@ namespace MySolution.Models
 {
     public class SellModel
     {
-        public SellModel(int code, int quantity, StatusModel status, ChannelModel channel)
+        public SellModel(uint prodCode, int soldQt, int status, int channel)
         {
-            Code = code;
-            Quantity = quantity;
-            Status = status;
-            Channel = channel;
-        }
+            this.ProdCode = prodCode;
+            this.SoldQt = soldQt;
+            this.Status = (StatusModel)status;
+            this.Channel = (ChannelModel)channel;
 
-        public int Code { get; set; }
-        public int Quantity { get; set; }
-        public StatusModel Status{ get; set; }
-        public ChannelModel Channel{ get; set; }
+            if (this.Status == Status.Completed || this.Status == Status.PaymentPending)
+            {
+                ////Total by ProdCode
+                ProductModel.SetTotalSoldByProdCode(this.ProdCode, soldQt);
+
+                //Total by Channel
+                ProductModel.SetTotalSoldByChannel(this.Channel, soldQt);
+            }
+            
+            
+        }
+        public uint ProdCode { get; }
+        public int SoldQt { get; }
+        public StatusModel Status { get; }
+        public ChannelModel Channel { get; }
 
     }
 }
