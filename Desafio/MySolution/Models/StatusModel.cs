0e3abbb45e22d60e0398dfd22cd11499a6351223
Igor Souza <souza.igor@protonmail.com>
2021-08-31 23:10:41 -0300

    Update StatusModel now has an extension method

--- Desafio/MySolution/Models/StatusModel.cs
+++ Desafio/MySolution/Models/StatusModel.cs
@@ -2,10 +2,31 @@ namespace MySolution.Models
 {
     public enum StatusModel
     {
-        PaymentConfirmed = 100,
-        PaymentPending = 102,
-        Canceled = 135,
-        NotConfirmed = 190,
-        UnknownError = 999
+            Completed = 100,
+            PaymentPending = 102,
+            Cancelled = 135,
+            NotCompleted = 190,
+            ErroDesconhecido = 999
     }
+    public static class StatusExtensions
+{
+    public static string ToFriendlyString(this StatusModel status)
+    {
+        switch(status)
+        {
+            case StatusModel.Completed:
+                return "venda confirmada e com pagamento ok";
+            case StatusModel.PaymentPending:
+                return "venda confirmada, mas com pagamento pendente";
+            case StatusModel.Cancelled:
+                return "venda cancelada";
+            case StatusModel.NotCompleted:
+                return "venda não finalizada no canal de vendas";
+            case StatusModel.ErroDesconhecido:
+                return "erro não identificado";
+            default:
+                return "";
+        }
+    }
+}
 }
