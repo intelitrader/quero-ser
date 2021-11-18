273e3072a6433219d075276c229a264a06b2e454
Alex Silva <alexaraujo_rj@yahoo.com.br>
2021-11-18 18:02:54 -0300

    new commit

--- Desafios DojoPuzzles/Caso 2/QuebraLinha.cs
+++ Desafios DojoPuzzles/Caso 2/QuebraLinha.cs
@@ -1,11 +1,13 @@
 ﻿
+
 using System;
 using System.Collections.Generic;
 
-namespace caso2
+namespace Caso2
 {
     class Program
     {
+        //Função para validar o tamanho da coluna em relação as palavras da frase
         static bool valida(string frase, int colunas)
         {
             string[] palavras = frase.Split(" ");
@@ -21,21 +23,25 @@ namespace caso2
             return true;
         }
 
-
+        //Função principal, quebra a frase em linhas
         static bool quebraLinha(string frase, int colunas)
         {
             List<string> linhas = new List<string>();
 
             string espaco = "";
 
+            //Primeira validação, palavras x coluna
             if (!valida(frase, colunas))
             {
                 Console.WriteLine("\nTamanho de coluna invalido! \n\nO tamanho da coluna deve ser maior \nque a quantidade de caracteres da maior palavra na frase\n");
                 return false;
             }
 
+            //Retira um pedaço da expressão passada por argumento e insere no vetor
+            //O laço de repetição acontece até que essa frase esteja em branco
             while (frase.Length > 0)
             {
+                //Importante verificação para a ultima palavra da expressão
                 if (frase.Length <= colunas)
                 {
                     linhas.Add(frase);
@@ -45,17 +51,23 @@ namespace caso2
 
                 if (frase.Length > colunas)
                 {
+                    //inicia uma variavel no indice correspondente ao valor da coluna informada
                     espaco = frase[colunas].ToString();
                 }
 
                 int cont = 0;
 
+                //Procura o espaço em branco mais próximo a esquerda ao indice indicado nas colunas
+                //caso a letra correspondente a esse respectivo indice nao seja vazio(espaço na frase)
+                //diminui um no indice até encontrar o espaço
                 while ((espaco != " ") & (frase != ""))
                 {
                     cont++;
                     espaco = frase[colunas - cont].ToString();
                 }
 
+                //caso a frase nao esteja vazia
+                //adiciona a parte da frase correspondente a lista criada que conterá todas as linhas formadas
                 if (frase != "")
                 {
                     string aux = frase.Substring(0, colunas - cont);
@@ -67,6 +79,7 @@ namespace caso2
 
             }
 
+            //Percorre a lista mostrando no console as linhas
             foreach (string element in linhas)
             {
                 Console.Write($"{element}\n");
@@ -105,3 +118,4 @@ namespace caso2
         }
     }
 }
+
--- Desafios DojoPuzzles/Caso 3/celular.cs
+++ Desafios DojoPuzzles/Caso 3/celular.cs
@@ -1,7 +1,4 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Caso3
 {
@@ -9,7 +6,7 @@ namespace Caso3
     {
         static void Main(string[] args)
         {
-           
+            //Dicionario com chave(Caracteres que devem ser procurados) e valor(Sera inputado caso a string seja encontrada)
             Dictionary<string, string> dic1 = new Dictionary<string, string>()
             {
                 { "ABC",  "2" },
@@ -23,38 +20,91 @@ namespace Caso3
                 { " ", "0" }
             };
 
-            string frase = "SEMPRE ACESSO O DOJOPUZZLES";
-            string cod = "";
-
-            for (var i = 0; i < frase.Length; i++)
+            //Funcao que transforma a string em codigo
+            static string transformCode(string frase, string cod, Dictionary<string, string> dic1, bool valid)
             {
-                foreach (var item in dic1)
+                //Percorre a frase recebida
+                for (var i = 0; i < frase.Length; i++)
                 {
-                    if ((item.Key).Contains(frase[i]))
+                    //variavel booleana, se tornara True quando o caracterer da frase for encontrado na chave do dicionario
+                    valid = false;
+
+                    //Percorre o dicionario
+                    foreach (var item in dic1)
                     {
-                        int ind = (item.Key).IndexOf(frase[i]);
-
-                        if (cod != "")
+                        //Se a chave do dicionario, contem a string da frase
+                        if ((item.Key).Contains(frase[i]))
                         {
+                            //valido que encontrou o caracterer dentro da chave do dicionario
+                            valid = true;
 
-                            if(cod[cod.Length - 1].ToString() == item.Value)
+                            //pega o indice da chave a qual o caracterer foi encontrado
+                            int ind = (item.Key).IndexOf(frase[i]);
+                            
+                            //variavel que iremos retornar caso tudo dê certo
+                            //verifica se é o primeiro codigo que estamos inserindo
+                            if (cod != "")
                             {
-                                cod = cod + "_" + string.Concat(Enumerable.Repeat(item.Value, ind + 1));
+
+                                //verifica se a ultima posicao da variavel cod
+                                //é igual ao valor da chave do diciario que percorremos
+                                if (cod[cod.Length - 1].ToString() == item.Value)
+                                {
+                                    //se sim, acrescenta a ele mesmo 
+                                    //underline + o valor da chave encontrado
+                                    //Enurable.Repeat(valor, quantidade de vezes)
+                                    cod = cod + "_" + string.Concat(Enumerable.Repeat(item.Value, ind + 1));
+                                }
+                                else
+                                {
+                                    //se nao, nao precisa adicionar o underline
+                                    cod = cod + string.Concat(Enumerable.Repeat(item.Value, ind + 1));
+                                }
                             }
                             else
                             {
+                                //se está verificando a primeira string da frase
+                                //código como o anterior
                                 cod = cod + string.Concat(Enumerable.Repeat(item.Value, ind + 1));
                             }
                         }
-                        else
-                        {
-                            cod = cod + string.Concat(Enumerable.Repeat(item.Value, ind + 1));
-                        }
                     }
+                    //se o caracterer da frase é encontrado no passo anterior
+                    //aqui deveria ser True
+                    //caso seja falso, foi digitado um caracterer invalido
+                    //dessa forma pode-se implementar alguma logica diferente para outros caracteres, caso necessario.
+                    if (!valid)
+                    {
+                        return false.ToString();
+                    }
+
                 }
+                //retorno da variavel com o codigo correspondente
+                return cod;
             }
 
-            Console.WriteLine(cod);
+            //string frase = "SEMPRE ACESSO O DOJOPUZZLES";
+            Console.WriteLine("\nDigite uma frase com no maximo 255 Caracteres");
+            Console.WriteLine("sem virgulas, acentos, numeros ou qualquer caracterer especial: \n");
+
+            //recebe a frase do usuario
+            string frase = Console.ReadLine().ToUpper();
+
+            //chama a funcao que vai transformar frase em codigo
+            string codigo = transformCode(frase, "", dic1, false);
+
+            if(codigo != "False")
+            {
+                Console.WriteLine("\nCodigo numerico: " + codigo + "\n");
+            }
+            else
+            {
+                Console.WriteLine("Caracter invalido na frase");
+            }
+            
+            Console.WriteLine("\n Aperte qualquer tecla para encerrar \n");
+            Console.ReadLine();
         }
     }
 }
+
