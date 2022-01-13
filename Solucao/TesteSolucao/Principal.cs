20cfca2d4067ee3e83782ff8a98d0f89ec7bb0a0
Gustavo1185 <gustavoanjos13@hotmail.com>
2022-01-12 23:20:14 -0300

    upload da solucao

---
+++ Solucao/TesteSolucao/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TesteSolucao;
+
+class Program
+{
+
+
+    static string caminhoprodutos = @"C:\Users\Administrador\Desktop\Teste\ConsoleTeste\CaseTeste1\c1_produtos.txt";
+    static string caminhovendas = @"C:\Users\Administrador\Desktop\Teste\ConsoleTeste\CaseTeste1\c1_vendas.txt";
+
+
+    static void Main()
+    {
+        List<Produto> ListaProdutos = new List<Produto>();
+        List<Vendas> ListaVendas = new List<Vendas>();
+
+        ListaProdutos = Produto.InserirProdutos(caminhoprodutos);
+        ListaVendas = Vendas.InserirVendas(caminhovendas);
+        Transfere.Example(ListaProdutos,ListaVendas);
+        RelatorioDivergencia.Divergencia(ListaProdutos, ListaVendas);
+        RelatorioCanais.Canais(ListaVendas);
+
+    }
+
+
+
+}
---
+++ Solucao/TesteSolucao/RelatorioDivergencia.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteSolucao
+{
+    internal class RelatorioDivergencia
+    {
+        public static async Task Divergencia(List<Produto> produtos, List<Vendas> vendas)
+        {
+
+            int linha = 0;
+            int verifica = 0;
+
+            using (var writer = new StreamWriter(@"C:\Users\Administrador\Desktop\Teste\TesteSolucao\divergencias.txt"))
+            {
+
+
+
+                foreach (Vendas v in vendas)
+                {
+                    linha++;
+                    verifica = 0;
+
+                    foreach (Produto p in produtos)
+                    {
+
+
+                        if (v.CodProduto == p.CodProduto)
+                        {
+                            verifica = 1;
+                            switch (v.SituacaoVenda)
+                            {
+                                case "135":
+                                    writer.Write("Linha " + linha + " - Venda cancelada" + "\n");
+                                    break;
+                                case "190":
+                                    writer.Write("Linha " + linha + " - Venda não finalizada" + "\n");
+                                    break;
+                                case "999":
+                                    writer.Write("Linha " + linha + " - Erro desconhecido. Acionar equipe de TI" + "\n");
+                                    break;
+
+
+                            }
+
+                        }
+
+                    }
+                        
+                        if (verifica==0)
+                        {
+
+                                writer.Write("Linha " + linha + " - Código de Produto não encontrado " + v.CodProduto + "\n");
+                        }
+
+                        
+
+                    }
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+
+
