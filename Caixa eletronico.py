aaec2e7377047b6251d1a46d2511fa2a3ee2e98b
André de J Santos <84469424+andrejsantos@users.noreply.github.com>
2022-05-16 17:42:05 -0300

    Dojo-Puzzles
    Desafios feito em python.
    Jokenpo.
    Caixa eletronico.
    FizzBuzz.

---
+++ Caixa eletronico.py
@@ -0,0 +1,24 @@
+print('='* 50)
+print('CAIXA 24H')
+print('='* 50)
+valor = int(input('Que valor você quer sacar?'))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('SAQUE CONCLUIDO, VOLTE SEMPRE.')
