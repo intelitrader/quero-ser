7b6f9385b16c1849e2bab6125dd9269fa7427ab2
henriqueweege <henriqueweege@gmail.com>
2022-06-20 18:09:32 -0300

    Criando minha solução
    Solução criada para o desafio.

---
+++ mysolution/InteliTraderSolution/InteliTraderSolutionPlus/Models/Reports/TotalChannelReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteliTraderSolutionPlus.Models.Reports
+{
+    public class TotalChannelReport
+    {
+        public string Description { get; set; }
+        public TotalChannelLine TotalChannelLine { get; set; }
+    }
+}
---
+++ mysolution/InteliTraderSolution/InteliTraderSolutionPlus/Reader/IProductReader.cs
@@ -0,0 +1,12 @@
+﻿
+using InteliTraderSolutionPlus.Models;
+
+namespace InteliTraderSolution.Reader
+{
+    public interface IProductReader
+    {
+        public IEnumerable<Product> Read();
+
+
+    }
+}
