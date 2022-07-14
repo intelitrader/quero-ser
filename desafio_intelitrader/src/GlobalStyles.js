2c4e6e4df11ff3740eae0b82d1a6420739d15822
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-07-14 10:41:00 -0300

    Criado projeto e feitos preparativos iniciais de ambiente

---
+++ desafio_intelitrader/src/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle = `
+    *{
+        margin: 0;
+        paddding: 0;
+        boxing-size: bording-box;
+        font-family: Arial, sans-serif;
+    }
+
+    body{
+        background-color: #0B0B3B;
+        color: white;
+    }
+`
---
+++ desafio_intelitrader/src/Routes.js
@@ -0,0 +1,9 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+export default function AppRoutes(){
+    <BrowserRouter>
+        <Routes>
+            <Route/>
+        </Routes>
+    </BrowserRouter>
+}
