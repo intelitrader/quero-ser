14ebd9ec87c9d6f5a641b7b47c158a1c770e8704
Matheus Marinho <math.marinho154@gmail.com>
2022-07-22 00:40:44 -0300

    feat: adição da pasta de css, adiciona novos elementos a pagina como botoes, o footer e a base da tabela

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import styled from '../Css/home.module.css';
 
 function Home() {
   const history = useHistory()
   return(
     <main>
-      <header>
+      <header className={ styled.header }>
         <Link to="/"><h1>INTELITRADER</h1></Link>
       </header>
       <div>
@@ -17,6 +18,9 @@ function Home() {
         <input type="file" accept="text/plain" className="input-vendas" />
       </div>
       <button onClick={ () => history.push('/table') }>Acessar Tabela</button>
+      <footer className={ styled.footer }>
+        <h2>Desenvolvido por <a href="https://www.linkedin.com/in/matheus-marinhodsp/">Matheus Marinho</a></h2>
+      </footer>
     </main>
   );
 };
