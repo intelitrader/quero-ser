31818c18dcdf0745c4d94fb66f5e5e4c3f07481e
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-08-04 12:37:59 -0300

    Feat: getters e setters para a classe ProductSold.

--- Resolucao/src/ProductSold.java
+++ Resolucao/src/ProductSold.java
@@ -6,4 +6,20 @@ public class ProductSold {
     this.product = product;
     this.amountSales = amoutSales;
   }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public int getAmountSales() {
+    return amountSales;
+  }
+
+  public void setAmountSales(int amountSales) {
+    this.amountSales = amountSales;
+  }
 }
