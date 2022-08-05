710e0d83572fd96340606d956060afb95c3ed37b
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-08-05 10:16:03 -0300

    Feat: cria classe Divergences para armazenar objetos criados da classe Divergence.

---
+++ Resolucao/src/Divergences.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Divergences {
+  ArrayList<Divergence> divergences;
+
+  public Divergences() {
+    this.divergences = new ArrayList<>();
+  }
+
+  public void addDivergence(Divergence divergence) {
+    this.divergences.add(divergence);
+  }
+}
