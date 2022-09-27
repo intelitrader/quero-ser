0ccb1c28c19d02be9e0c265422332b6ba5990a66
roberttmello <roberttmello@protonmail.com>
2022-09-27 11:51:30 -0300

    Completed divergencias file feature

---
+++ Desafio/my-solution/divergencias.js
@@ -0,0 +1,60 @@
+const { readFileSync, writeFileSync } = require("fs");
+const productsData = readFileSync("./data/c2_produtos.txt", "utf8");
+const salesData = readFileSync("./data/c2_vendas.txt", "utf8");
+
+//Converts a string of data into an array, separating it with "\n" and ";"...
+const convertStringDataToArray = (stringData) => {
+  const dataArray = stringData.split("\n");
+  return dataArray.map((data) => data.split(";"));
+};
+
+//Converting string data of the products into array
+productsDataArray = convertStringDataToArray(productsData);
+
+// Generating an array with only the product codes.
+const productCodesArray = [];
+productsDataArray.forEach((product) => {
+  productCodesArray.push(Number(product[0]));
+});
+
+//Converting string data of the sales into array
+salesDataInfo = convertStringDataToArray(salesData);
+
+let divergencesInfo = "";
+let sellerSales = 0;
+let websiteSales = 0;
+let androidAppSales = 0;
+let iphoneAppSales = 0;
+
+for (const sale of salesDataInfo) {
+  //Test to identify non-existent code in the product list.
+  if (!productCodesArray.includes(Number(sale[0]))) {
+    divergencesInfo += `Linha ${
+      salesDataInfo.indexOf(sale) + 1
+    } - Código de produto não encontrado ${sale[0]}\n`;
+  } else {
+    // Test to identify canceled sale
+    if (sale[2] === "135") {
+      divergencesInfo += `Linha ${
+        salesDataInfo.indexOf(sale) + 1
+      } - Venda cancelada\n`;
+      continue;
+    }
+    // Test to identify unfineshed sale
+    if (sale[2] === "190") {
+      divergencesInfo += `Linha ${
+        salesDataInfo.indexOf(sale) + 1
+      } - Venda não finalizada\n`;
+      continue;
+    }
+    // Test to identify unknown error.
+    if (sale[2] === "999") {
+      divergencesInfo += `Linha ${
+        salesDataInfo.indexOf(sale) + 1
+      } - Erro desconhecido. Acionar equipe de TI\n`;
+    }
+  }
+}
+
+//Generating the divergence file
+writeFileSync("output/divergencias.txt", divergencesInfo);
