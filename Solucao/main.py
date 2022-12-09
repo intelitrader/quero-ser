444a5a2a552cd9722c8ea00723863dabd6b05c21
Henrique Castro <williancostajr@hotmail.com>
2022-12-08 22:48:46 -0300

    Continuação projeto quero ser

--- Solucao/main.py
+++ Solucao/main.py
@@ -9,42 +9,62 @@
 # a quantidade de itens que devem ser enviados do Armazém para o Centro Operacional.
 """print((open("c1_produtos.txt")).read())
  print((open("c1_vendas.txt")).read())"""
+class Produto:
+    def __init__(self, idprod, qntest, min):
+        self.idprod = idprod
+        self.qntest = qntest
+        self.min = min
+totcanais = {}
 estoque = []
-idprod = []
+vendasprod = {}
 vendas = open("c1_vendas.txt", "r")
-vendaslst = []
+lstprod = []
 pro = open('c1_produtos.txt')
-vp1 = vp2 = vp3 = vp4 = vp5 = 0
-# p1 = vendas produto 1 (16320)
-# p2 = vendas produto 2 (23400)
-# p3 = vendas produto 3 (26440)
-# p4 = vendas produto 4 (28790)
-# p5 = vemdas produto 5 (36540)
 for produto in pro:
-    estoque.append((produto.split(';')))
-    idprod.append(produto.split(';')[0])
-    for linha in vendas:
-        v = linha.split(';')
-        if v[2] == '100' or v[2] == '102':  # se as vendas forem confirmadas
-            # if v[0] == idprod[int(produto)]:
-            #     vendalst = int(vendaslst[int(produto)]) + int(v[1])
-            #     vendaslst.append(vendalst)
-            if v[0] == idprod[0]:  # se as vendas confirmadas tivere o codigo do p1
-                vp1 += int(v[1])
-            elif v[0] == idprod[1]:  # se as vendas confirmadas tivere o codigo do p2
-                vp2 += int(v[1])
-            elif v[0] == idprod[2]:  # se as vendas confirmadas tivere o codigo do p3
-                vp3 += int(v[1])
-            elif v[0] == idprod[3]:  # se as vendas confirmadas tivere o codigo do p4
-                vp4 += int(v[1])
-            elif v[0] == idprod[4]:  # se as vendas confirmadas tivere o codigo do p5
-                vp5 += int(v[1])
-print(vendaslst)
-print(f"""
-Produto  QtCO    QtMin  QtVendas   Estq.após    Necess.     Transf. de
-					                Vendas		                Arm p/ CO
-{idprod[0]}     {estoque[0][1]}	 {int(estoque[0][2])}   {vp1}	        {int(estoque[0][1]) -vp1 }          {0 if (int(estoque[0][1]) -vp1) > int(estoque[0][2]) else int(estoque[0][2]) - (int(estoque[0][1]) -vp1)}	        0
-{idprod[1]}     {estoque[1][1]}	 {int(estoque[1][2])}   {vp2}	        {int(estoque[1][1]) -vp2 }          {0 if (int(estoque[1][1]) -vp2) > int(estoque[1][2]) else int(estoque[1][2]) - (int(estoque[1][1]) -vp2)}	        10
-{idprod[2]}     {estoque[2][1]}	 {int(estoque[2][2])}   {vp3}	        {int(estoque[2][1]) -vp3 }         {0 if (int(estoque[2][1]) -vp3) > int(estoque[2][2]) else int(estoque[2][2]) - (int(estoque[2][1]) -vp3)}	        0
-{idprod[3]}     {estoque[3][1]}	 {int(estoque[3][2])}   {vp4}	        {int(estoque[3][1]) -vp4 }           {0 if (int(estoque[3][1]) -vp4) > int(estoque[3][2]) else int(estoque[3][2]) - (int(estoque[3][1]) -vp4)}	        85
-{idprod[4]}     {estoque[4][1]}	 {int(estoque[4][2])}   {vp5}          {int(estoque[4][1]) -vp5 }         {0 if (int(estoque[4][1]) -vp5) > int(estoque[4][2]) else int(estoque[4][2]) - (int(estoque[4][1]) -vp5)}        286""")
+    infprod = produto.split(';')
+    estoque.append(Produto(infprod[0], infprod[1], infprod[2]))
+    idproduto = infprod[0]
+    vendasprod.update({f'{idproduto}': 0})
+c = 1
+divergencias = open('divergencias.txt', 'w+')
+for venda in vendas:
+    informacoes = venda.split(';')
+    if informacoes[2] == '999':
+        divergencias.writelines(f"""Linha {c} – Erro desconhecido. Acionar equipe de TI\n""")
+    elif informacoes[2] == '190':
+        divergencias.writelines(f"""Linha {c} – Venda não finalizada\n""")
+    elif not vendasprod.keys().__contains__(informacoes[0]):
+        divergencias.writelines(f"""Linha {c} – Código de Produto não encontrado {informacoes[0]}\n""")
+    elif informacoes[2] == '135':
+        divergencias.writelines(f"""Linha {c} – Venda cancelada\n""")
+    if informacoes[2] == '100' or informacoes[2] == '102':  # se as vendas forem confirmadas
+        if not totcanais.keys().__contains__(informacoes[3]):
+            totcanais.update(({f'{informacoes[3]}': 0}))
+        if totcanais.keys().__contains__(informacoes[3]):
+            valor = totcanais.get(informacoes[3])
+            totcanais.update({f"{informacoes[3]}": valor + int(informacoes[1])})
+        if vendasprod.keys().__contains__(informacoes[0]):
+            valor = vendasprod.get(informacoes[0])
+            vendasprod.update({f"{informacoes[0]}": valor + int(informacoes[1])})
+    c += 1
+transfere = open('transfere.txt', 'w+')
+transfere.writelines("""
+Necessidade de Transferencia Armazem para CO
+
+Produto  QtCO  QtMin  QtVendas  Estq.após  Necess.  Transf. de
+                                Vendas              Arm p/ CO\n""")
+for chave, valor in vendasprod.items():
+    qtco = int(next(produto.qntest for produto in estoque if produto.idprod == chave))
+    qtmin = int(next(produto.min for produto in estoque if produto.idprod == chave))
+    restante = qtco - int(valor)
+    necessario = 0 if restante > qtmin else qtmin - restante
+    nec_trans = 10 if 0 < necessario < 10 else necessario
+    str(transfere.writelines(
+        f"""{chave:}    {qtco:<4}  {qtmin:<4}    {valor:<4}     {restante:<4}       {necessario:<4}     {nec_trans}\n"""))
+totcanaistext = open('totcanal.txt', 'w+')
+totcanaistext.writelines(f"Quantidades de Vendas por canal\n"
+                         f"\n"
+                         f"1 - Representantes		    {list(totcanais.values())[0]}\n"
+                         f"2 - Website			        {list(totcanais.values())[2]}\n"
+                         f"3 - App móvel Android		{list(totcanais.values())[1]}\n"
+                         f"4 - App móvel iPhone		{list(totcanais.values())[3]}")
