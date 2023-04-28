35eea6fac11e8b0cea9e75e07610da713d36b218
fabiomagina <fabiomagina@gmail.com>
2023-04-28 16:37:15 -0300

    solução desafio

---
+++ Desafio/solução JS/pathConfig.js
@@ -0,0 +1,8 @@
+export const DATA__PATH = {
+  produtos: './data/produtos.txt',
+  vendas: './data/vendas.txt'
+}
+
+export const DIVERGENCY__PATH = './data/DIVERGENCIAS.txt'
+export const SALES__CHANNEL__REPORT__PATH = './data/TOTCANAIS.txt'
+export const TRANSFER__REPORT__PATH = './data/transfere.txt'
