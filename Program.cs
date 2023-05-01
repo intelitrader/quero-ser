3d146d678f02c76b7dae95077660b6a1301e938d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 20:03:59 -0300

    feat: Register ISalesRepository and ImportSaleUseCase in dependency injection

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using quero_ser.model;
 using quero_ser.repositories;
 using quero_ser.repositories.implementation;
 using quero_ser.useCase.importProduct;
+using quero_ser.useCase.importSale;
 
 namespace quero_ser_master;
 
@@ -16,10 +17,15 @@ class Program
         .AddScoped<IProductsRepository, ProductsRepository>()
         .AddScoped<ImportProductUseCase>()
 
+        // Vendas
+        .AddScoped<ISalesRepository, SalesRepository>()
+        .AddScoped<ImportSaleUseCase>()
+
         .BuildServiceProvider();
 
-
         ImportProductUseCase importProductUseCase = serviceProvider.GetService<ImportProductUseCase>()!;
+
+        ImportSaleUseCase importSaleUseCase = serviceProvider.GetService<ImportSaleUseCase>()!;
 
         string diretorios = $"{System.Environment.CurrentDirectory}/Desafio";
         string[] files = Directory.GetDirectories(diretorios);
@@ -31,6 +37,8 @@ class Program
                 string nameFile = Path.GetFileName(files[i]);
 
                 List<Product> productsList = importProductUseCase.execute(diretorios, nameFile);
+
+                List<Sale> salesList = importSaleUseCase.execute(diretorios, nameFile);
 
             }
 
