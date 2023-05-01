2e46841be138494b5805fdc33dc412f7c4d64080
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:19:40 -0300

    feat: create ImportProductUseCase in usecase/importProduct

---
+++ useCase/importProduct/ImportProductUseCase.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quero_ser.useCase.importProduct
+{
+    public class ImportProductUseCase
+    {
+        
+    }
+}
