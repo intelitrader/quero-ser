c5eb685516cf4e47d6bde9e961baecabb13e1ffe
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:56:45 -0300

    feat: Added SalesRepository class implementing ISalesRepository with ReadFile and FormatSaleListFile methods

---
+++ repositories/implementation/SalesRepository.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using quero_ser.model;
+
+namespace quero_ser.repositories.implementation
+{
+    public class SalesRepository : ISalesRepository
+    {
+        public List<Sale> FormatSaleListFile(string[] lines)
+        {
+            List<Sale> _salesList = new List<Sale> { };
+
+            foreach (string line in lines)
+            {
+                string[] formatLine = line.Split(';');
+
+                var newProduct = new Sale()
+                {
+                    productCode = uint.Parse(formatLine[0]),
+                    quantitySale = int.Parse(formatLine[1]),
+                    saleSituation = int.Parse(formatLine[2]),
+                    saleChannel = int.Parse(formatLine[3])
+                };
+
+                _salesList.Add(newProduct);
+            }
+
+            return _salesList;
+        }
+
+        public string[] ReadFile(string pathFile)
+        {
+            string[] lines = File.ReadAllLines(pathFile);
+
+            return lines;
+        }
+    }
+}
