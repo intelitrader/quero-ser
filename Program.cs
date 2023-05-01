708594ad9a1290c9f64c8371e7d8eada916a798a
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:34:28 -0300

    feat: add dependency injection and importProductUseCase execution in Program class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using quero_ser.model;
+using quero_ser.repositories;
+using quero_ser.repositories.implementation;
+using quero_ser.useCase.importProduct;
+
+namespace quero_ser_master;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+        // Produtos
+        .AddScoped<IProductsRepository, ProductsRepository>()
+        .AddScoped<ImportProductUseCase>()
+
+        .BuildServiceProvider();
+
+
+        ImportProductUseCase importProductUseCase = serviceProvider.GetService<ImportProductUseCase>()!;
+
+        string diretorios = $"{System.Environment.CurrentDirectory}/Desafio";
+        string[] files = Directory.GetDirectories(diretorios);
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            if (files[i].Contains("Caso"))
+            {
+                string nameFile = Path.GetFileName(files[i]);
+
+                List<Product> productsList = importProductUseCase.execute(diretorios, nameFile);
+
+            }
+
+        }
+    }
+}
