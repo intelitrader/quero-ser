ea416227d1feb383555b7b87c9a069a4f0775362
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:15:57 -0300

    feat: implement IProductRepository with ReadFile logic

--- repositories/implementation/ProductsRepository.cs
+++ repositories/implementation/ProductsRepository.cs
@@ -1,7 +1,12 @@
 namespace quero_ser.repositories.implementation
 {
-    public class ProductsRepository
+    public class ProductsRepository : IProductsRepository
     {
+        public string[] ReadFile(string pathFile)
+        {
+            string[] lines = File.ReadAllLines(pathFile);
 
+            return lines;
+        }
     }
 }
