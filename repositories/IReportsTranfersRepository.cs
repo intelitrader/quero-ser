266fe21252d4ae60db7ef7d27e62731d6dea952d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 20:15:37 -0300

    feat: Added interface IReportsTransfersRepository with method FilterProductsSales.

---
+++ repositories/IReportsTranfersRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using quero_ser.model;
+
+namespace quero_ser.repositories
+{
+    public interface IReportsTranfersRepository
+    {
+        public List<Sale> FilterProductsSales(Product product, List<Sale> salesList);
+
+    }
+}
