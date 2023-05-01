21ca8766d547acf6896859feb52bbf957ad85a11
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:43:01 -0300

    feat: implement FormatProductListFile method in ProductsRepository

--- repositories/implementation/ProductsRepository.cs
+++ repositories/implementation/ProductsRepository.cs
@@ -1,3 +1,5 @@
+using quero_ser.model;
+
 namespace quero_ser.repositories.implementation
 {
     public class ProductsRepository : IProductsRepository
@@ -7,6 +9,24 @@ namespace quero_ser.repositories.implementation
             string[] lines = File.ReadAllLines(pathFile);
 
             return lines;
+        }
+        public List<Product> FormatProductListFile(string[] lines)
+        {
+            List<Product> productsList = new List<Product> { };
+            foreach (string line in lines)
+            {
+                string[] formatLine = line.Split(';');
+
+                Product newProduct = new Product()
+                {
+                    productCode = uint.Parse(formatLine[0]),
+                    quantityInStockBeginningPeriod = int.Parse(formatLine[1]),
+                    minStockCenterOperation = int.Parse(formatLine[2]),
+                };
+
+                productsList.Add(newProduct);
+            }
+            return productsList;
         }
     }
 }
