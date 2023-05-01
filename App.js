f714455316a78c386caf4173a9abd9d28bd4611b
unknown <antunespedro222@gmail.com>
2023-05-01 17:56:45 -0300

    feat: funcao de somar as vendas de cada produto

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
 // var readVendas = require("./Functions/readVendas.js");
-import montaProduto from "./Functions/Tratamento/montaProduto.js";
-import montaVenda from "./Functions/Tratamento/montaVenda.js";
+import montaTabela1 from "./Functions/montaTabela/montaTransfere.js";
 
-const teste = montaProduto('./Entrada/c1_produtos.txt');
-
-teste.forEach(element => console.log(element));
-// console.log(teste)
+montaTabela1();
---
+++ Functions/montaTabela/montaTransfere.js
@@ -0,0 +1,32 @@
+import montaProduto from "../Tratamento/montaProduto.js";
+import montaVenda from "../Tratamento/montaVenda.js";
+
+export default function montaTransfere() {
+  const ArrayProdutos = montaProduto("./Entrada/c1_produtos.txt");
+  const ArrayVendas = montaVenda("./Entrada/c1_vendas.txt");
+
+  let produtosVendidos = {};
+
+  // inicia cada posição com 0
+  ArrayProdutos.forEach((produto) => {
+    produtosVendidos[produto.codigo] = 0;
+  });
+
+  ArrayVendas.forEach((venda) => {
+    const codigoVenda = parseInt(venda.codigoProduto, 10);
+    const quantidade = parseInt(venda.quantidade, 10);
+
+    const teste = ArrayProdutos.find(
+      (produto) => produto.codigo == codigoVenda
+    );
+
+    // se caso o .txt tiver alguma linha vazia
+    if (teste) {
+      produtosVendidos[codigoVenda] += quantidade;
+    }
+
+    // console.log(produtosVendidos);
+  });
+
+  console.log(produtosVendidos);
+}
