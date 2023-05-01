2f13e018091da9f0191efad5603902049a367602
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 20:40:47 -0300

    feat: add method CreateFileTransfer in interface IReportsTransferRepository

--- repositories/IReportsTranfersRepository.cs
+++ repositories/IReportsTranfersRepository.cs
@@ -8,6 +8,7 @@ namespace quero_ser.repositories
 {
     public interface IReportsTranfersRepository
     {
+        public void CreateFileTransfer(string filePath, List<Transfer> transferList);
         public List<Sale> FilterProductsSales(Product product, List<Sale> salesList);
         public int TotalProductSales(List<Sale> productSaleList);
 
