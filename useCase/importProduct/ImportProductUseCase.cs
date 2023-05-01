fab5b738cb8b3a8a800da3bc0d3b1b2f923e3071
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:28:55 -0300

    feat: add dependency injection and execute method to ImportProductUseCase

--- useCase/importProduct/ImportProductUseCase.cs
+++ useCase/importProduct/ImportProductUseCase.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using quero_ser.model;
+using quero_ser.repositories;
 
 namespace quero_ser.useCase.importProduct
 {
     public class ImportProductUseCase
     {
-        
+        private readonly IProductsRepository _productsRepository;
+
+        public ImportProductUseCase(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        public List<Product> execute(string pathFolder, string folderName)
+        {
+
+            string numberFile = folderName.Split(' ').Last();
+            string filePath = $"{pathFolder}/{folderName}/c{numberFile}_produtos.txt";
+
+            string[] lines = _productsRepository.ReadFile(filePath);
+
+        }
     }
 }
