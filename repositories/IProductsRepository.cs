df6b2070dac7f90b73068dffdfcb80a9db7a7994
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 19:38:43 -0300

    feat: add method FormatProductListFile to IProductRepository

--- repositories/IProductsRepository.cs
+++ repositories/IProductsRepository.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using quero_ser.model;
 
 namespace quero_ser.repositories
 {
     public interface IProductsRepository
     {
         public string[] ReadFile(string path);
+        public List<Product> FormatProductListFile(string[] lines);
+
     }
 }
