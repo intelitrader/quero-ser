69281f39c267dd3f895fd1d19069ecd509426d82
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 21:01:09 -0300

    feat: create model  Divergence

---
+++ model/Divergence.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quero_ser.model
+{
+    public class Divergence
+    {
+        public int numberLine { get; set; }
+        public string? message { get; set; }
+    }
+}
