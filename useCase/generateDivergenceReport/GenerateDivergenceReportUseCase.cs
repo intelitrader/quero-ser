b9b1ab2ccb3b90d1c1a0a5d0423b2f415b9d044f
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 22:19:48 -0300

    feat: add GenerateDivergenceReportUseCase

--- useCase/generateDivergenceReport/GenerateDivergenceReportUseCase.cs
+++ useCase/generateDivergenceReport/GenerateDivergenceReportUseCase.cs
@@ -18,11 +18,23 @@ namespace quero_ser.useCase.generateDivergenceReport
 
         public void execute(List<Sale> salesList, List<Product> productsList, string pathFolder, string folderName)
         {
-
             string createNameFile = "DIVERGENCIAS.txt";
             string filePathSave = $"{pathFolder}/{folderName}/{createNameFile}";
 
-            Console.WriteLine("GenerateDivergenceReportUseCase");
+            List<Divergence> divergencesList = new List<Divergence>();
+
+            for (int i = 0; i < salesList.Count; i++)
+            {
+                divergencesList.AddRange(_reportsDivergencesRepository.FilterNotFoundProducts(productsList, salesList[i], i));
+                divergencesList.AddRange(_reportsDivergencesRepository.FilterCanceledSales(productsList, salesList[i], i));
+                divergencesList.AddRange(_reportsDivergencesRepository.FilterIncompleteSales(productsList, salesList[i], i));
+                divergencesList.AddRange(_reportsDivergencesRepository.FilterUnknownErrors(productsList, salesList[i], i));
+            }
+
+            divergencesList.OrderBy(divergence => divergence.numberLine).ToList();
+
+            _reportsDivergencesRepository.CreateFileDivergences(filePathSave, divergencesList);
+
         }
     }
 }
