62b3b616a82d7e42097c1f935bcfe591ab14f4f5
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-01 22:04:37 -0300

    feat: add interface FilterCanceledSales, FilterIncompleteSales,FilterUnknownErrors adn CreateFileDivergences

--- repositories/IReportsDivergencesRepository.cs
+++ repositories/IReportsDivergencesRepository.cs
@@ -9,5 +9,10 @@ namespace quero_ser.repositories
     public interface IReportsDivergencesRepository
     {
         List<Divergence> FilterNotFoundProducts(List<Product> productsList, Sale sale, int line);
+        List<Divergence> FilterCanceledSales(List<Product> productsList, Sale sale, int line);
+        List<Divergence> FilterIncompleteSales(List<Product> productsList, Sale sale, int line);
+        List<Divergence> FilterUnknownErrors(List<Product> productsList, Sale sale, int line);
+        void CreateFileDivergences(string filePath, List<Divergence> divergencesList);
+
     }
 }
