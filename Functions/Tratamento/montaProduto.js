1f73a6422120dc894b617b356197f8280d9ff6a6
unknown <antunespedro222@gmail.com>
2023-05-02 22:57:10 -0300

    fix: melhoria na legibilidade do codigo

--- Functions/Tratamento/montaProduto.js
+++ Functions/Tratamento/montaProduto.js
@@ -7,17 +7,30 @@ export default function montaProduto(url) {
 
   const produtosTratados = rawProdutos.split("\r\n");
 
-  const linhaProdutos = [];
-
-  for (let i = 0; i < produtosTratados.length; i++) {
-    const linha = [];
-    linha[i] = produtosTratados[i].split(";");
-    linhaProdutos[i] = new Produto(
-      parseInt(linha[i][0], 10),
-      parseInt(linha[i][1], 10),
-      parseInt(linha[i][2], 10)
+  const linhaProdutos = produtosTratados.map((produto) => {
+    const [
+      codigo,
+      quantidade,
+      quantidadeMinima
+    ] = produto.split(";");
+    return new Produto(
+      parseInt(codigo, 10),
+      parseInt(quantidade, 10),
+      parseInt(quantidadeMinima, 10)
     );
-  }
+  });
+
+  // const linhaProdutos = [];
+
+  // for (let i = 0; i < produtosTratados.length; i++) {
+  //   const linha = [];
+  //   linha[i] = produtosTratados[i].split(";");
+  //   linhaProdutos[i] = new Produto(
+  //     parseInt(dssdada, 10),
+  //     parseInt(linha[i][1], 10),
+  //     parseInt(linha[i][2], 10)
+  //   );
+  // }
 
   return linhaProdutos;
 }
--- Functions/Tratamento/montaVenda.js
+++ Functions/Tratamento/montaVenda.js
@@ -7,18 +7,15 @@ export default function montaVenda(url) {
 
   const vendasTratadas = rawVendas.split("\r\n");
 
-  const linhaVendas = [];
-
-  for (let i = 0; i < vendasTratadas.length; i++) {
-    const linha = [];
-    linha[i] = vendasTratadas[i].split(";");
-    linhaVendas[i] = new Venda(
-      parseInt(linha[i][0], 10),
-      parseInt(linha[i][1], 10),
-      parseInt(linha[i][2], 10),
-      parseInt(linha[i][3], 10)
+  const linhaVendas = vendasTratadas.map((linha) => {
+    const [codigo, quantidade, situacao, canal] = linha.split(";");
+    return new Venda(
+      parseInt(codigo, 10),
+      parseInt(quantidade, 10),
+      parseInt(situacao, 10),
+      parseInt(canal, 10)
     );
-  }
+  });
 
   return linhaVendas;
 }
