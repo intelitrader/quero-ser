0504518fff387a6827116910b6c0c31a2868e639
unknown <antunespedro222@gmail.com>
2023-05-02 21:23:56 -0300

    fix: linhas vazias no arquivo de entrada de produto causam erro

--- Class/Produto.js
+++ Class/Produto.js
@@ -1,8 +1,8 @@
 export default class Produto {
     constructor(codigo, quantidade, quantidadeMinima, vendas, estoque, necessidade, transferencia){
-        this.codigo = codigo ?? 0;
-        this.quantidade = quantidade ?? 0;
-        this.quantidadeMinima = quantidadeMinima ?? 0;
+        this.codigo = isNaN(codigo) ? 0 : codigo;
+        this.quantidade = isNaN(quantidade) ? 0 : quantidade;
+        this.quantidadeMinima = isNaN(quantidadeMinima) ? 0 : quantidadeMinima;
         this.vendas = vendas ?? 0;
         this.estoque = estoque ?? 0;
         this.necessidade = necessidade ?? 0;
