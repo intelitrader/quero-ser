757b8a3b2dda8ff1a2ec8da9cacd05d7a0b620e2
unknown <antunespedro222@gmail.com>
2023-05-03 17:01:26 -0300

    fix: erro de codigo acima de erro inexistente

--- Functions/montaTabela/somaVendas.js
+++ Functions/montaTabela/somaVendas.js
@@ -10,7 +10,7 @@ export default function somaVendas(ArrayProdutos, ArrayVendas, cn) {
   });
 
   // para saber a linha da divergencia
-  let count = 0;
+  let contPagina = 0;
 
   ArrayVendas.forEach((venda) => {
     const codigoVenda = venda.codigoProduto;
@@ -20,39 +20,52 @@ export default function somaVendas(ArrayProdutos, ArrayVendas, cn) {
       (produto) => produto.codigo == codigoVenda
     );
 
-    count++;
+    contPagina++;
 
-    // se caso o find nao retornar algo
-    if (vendaProduto) {
-      // chega a situacao da venda
-      switch (venda.situacao) {
-        case 100:
-        case 102:
-          produtosVendidos[codigoVenda] += quantidade;
-          break;
-        case 135:
-          divergencias.push("Linha " + count + " - Venda cancelada");
-          break;
-        case 190:
-          divergencias.push("Linha " + count + " - Venda não finalizada");
-          break;
-        default:
+    // essa condicao so existe para o arquivo de saida ficar igual ao exemplo
+    // originalmente primeiro verifica se o codigo de produto existe e depois se a situacao e 999
+    // dando o erro 'codigo inexistente' e nao 'erro nao identificado'
+    if (venda.situacao !== 999) {
+      // se caso o find nao retornar algo
+      if (vendaProduto) {
+        // chega a situacao da venda
+        switch (venda.situacao) {
+          case 100:
+          case 102:
+            produtosVendidos[codigoVenda] += quantidade;
+            break;
+          case 135:
+            divergencias.push("Linha " + contPagina + " - Venda cancelada");
+            break;
+          case 190:
+            divergencias.push(
+              "Linha " + contPagina + " - Venda não finalizada"
+            );
+            break;
+          default:
+            divergencias.push(
+              "Linha " +
+                contPagina +
+                " - Erro desconhecido. Acionar equipe de TI"
+            );
+            break;
+        }
+      } else {
+        // linhas vazias no arquivo entrada de venda
+        if (!isNaN(venda.codigoProduto)) {
+          // mostra a linha com o erro
           divergencias.push(
-            "Linha " + count + " - Erro desconhecido. Acionar equipe de TI"
+            "Linha " +
+              contPagina +
+              " - Código de Produto não encontrado " +
+              venda.codigoProduto
           );
-          break;
+        }
       }
     } else {
-      // linhas vazias no arquivo entrada de venda
-      if (!isNaN(venda.codigoProduto)) {
-        // mostra a linha com o erro
-        divergencias.push(
-          "Linha " +
-            count +
-            " - Código de Produto não encontrado " +
-            venda.codigoProduto
-        );
-      }
+      divergencias.push(
+        "Linha " + contPagina + " - Erro desconhecido. Acionar equipe de TI"
+      );
     }
   });
 
