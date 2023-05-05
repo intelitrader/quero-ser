db528408c23a171d330486631c3d7e06fadf5f4a
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-05-05 01:32:16 -0300

    fix: Fixing code

--- divergencesReport.js
+++ divergencesReport.js
@@ -2,24 +2,24 @@ import fs from "node:fs"
 import { productsArrayResult } from "./readingFiles.js";
 import { salesArrayResult } from "./readingFiles.js";
 
-const sales = salesArrayResult
+const sales = salesArrayResult;
 const products = productsArrayResult
 
 const divergences = [];
 
 for (const [line, sale] of Object.entries(sales)){
-    if (!products[sale.productCode]){
-        divergences.push(`Linha ${line} - Código do produto não encontrado ${sale.productCode}`)
+  if (!products[sales.productCode]){
+      divergences.push(`Linha ${line} - Código do produto não encontrado ${sale.productCode}`)
+  }
+  if (sales.sellSituation === '135') {
+      divergences.push(`Linha ${line} - Venda cancelada`);
     }
-    if (sale.sellSituation === '135') {
-        divergences.push(`Linha ${line} - Venda cancelada`);
-      }
-    if (sale.sellSituation === '190') {
-        divergences.push(`Linha ${line} - Venda não finalizada`);
-      }
-    if (sale.sellSituation === '999') {
-        divergences.push(`Linha ${line} - Erro desconhecido. Acionar equipe de TI.`);
-      }
+  if (sales.sellSituation === '190') {
+      divergences.push(`Linha ${line} - Venda não finalizada`);
+    }
+  if (sales.sellSituation === '999') {
+      divergences.push(`Linha ${line} - Erro desconhecido. Acionar equipe de TI.`);
+    }
 }
 
 fs.writeFileSync("DIVERGENCIAS.TXT", divergences.join("\n"), "utf-8")
