0a46660b5c4fff031b4410522b24ad37559d6206
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-05-05 12:51:30 -0300

    feat: Finishing logic of problem and creating .txt file

--- divergencesReport.js
+++ divergencesReport.js
@@ -1,26 +1,66 @@
-import fs from "node:fs"
-import { productsArrayResult } from "./readingFiles.js";
-import { salesArrayResult } from "./readingFiles.js";
+import fs from "node:fs";
+
+const salesFile = fs.readFileSync(
+  "./Desafio/Caso de teste 1/c1_vendas.txt",
+  "utf-8"
+);
+const salesArray = salesFile.split("\r\n");
+
+const salesArrayResult = [];
+
+salesArray.forEach((line, index) => {
+  const [productCode, QtSales, sellSituation, channel] = line.split(";");
+  if (
+    sellSituation === "100" ||
+    sellSituation === "102" ||
+    sellSituation === "135" ||
+    sellSituation === "190" ||
+    sellSituation === "999"
+  ) {
+    salesArrayResult[index + 1] = {
+      productCode,
+      QtSales,
+      sellSituation,
+      channel,
+    };
+  }
+  return salesArrayResult;
+});
+
+const productsFile = fs.readFileSync(
+  "./Desafio/Caso de teste 1/c1_produtos.txt",
+  "utf8"
+);
+const productsArray = productsFile.split("\r\n");
+const productsArrayResult = [];
+
+productsArray.forEach((line, index) => {
+  const [productCode, QtCO, QtMin] = line.split(";");
+  productsArrayResult[index + 1] = { productCode, QtCO, QtMin };
+  return productsArrayResult;
+});
 
 const sales = salesArrayResult;
-const products = productsArrayResult
+const products = productsArrayResult;
 
 const divergences = [];
 
-for (const [line, sale] of Object.entries(sales)){
-  if (!products[sales.productCode]){
-      divergences.push(`Linha ${line} - Código do produto não encontrado ${sale.productCode}`)
+for (const [line, sale] of Object.entries(sales)) {
+  if (!products[sale.productCode]) {
+    divergences.push();
   }
-  if (sales.sellSituation === '135') {
-      divergences.push(`Linha ${line} - Venda cancelada`);
-    }
-  if (sales.sellSituation === '190') {
-      divergences.push(`Linha ${line} - Venda não finalizada`);
-    }
-  if (sales.sellSituation === '999') {
-      divergences.push(`Linha ${line} - Erro desconhecido. Acionar equipe de TI.`);
-    }
+  if (sale.sellSituation === "135") {
+    divergences.push(`Linha ${line} - Venda cancelada`);
+  }
+  if (sale.sellSituation === "190") {
+    divergences.push(`Linha ${line} - Venda não finalizada`);
+  }
+  if (sale.sellSituation === "999") {
+    divergences.push(
+      `Linha ${line} - Erro desconhecido. Acionar equipe de TI.`
+    );
+  }
 }
 
-fs.writeFileSync("DIVERGENCIAS.TXT", divergences.join("\n"), "utf-8")
-console.log(divergences)
+fs.writeFileSync("DIVERGENCIAS.TXT", divergences.join("\n"), "utf-8");
+console.log(divergences);
